7c1af0baf5498284dd66dacfeee95766ae56363b
fileaccent <x863557386@163.com>
2019-05-02 15:12:55 +0800

    修改bug

--- js/slideControl.js
+++ js/slideControl.js
@@ -58,6 +58,23 @@
             default:
         }
     }, false);
-    $('body').on('touchmove', function (event) {event.preventDefault();});
-    or
-    document.addEventListener('touchmove', function(e){e.preventDefault()}, false);
+    var overscroll = function (el) {
+        el.addEventListener('touchstart', function () {
+            var top = el.scrollTop;
+            var totalScroll = el.scrollHeight;
+            var currentScroll = top + el.offsetHeight;
+            if (top === 0) {
+                el.scrollTop = 1;
+            } else if (currentScroll === totalScroll) {
+                el.scrollTop = top - 1;
+            }
+        });
+        el.addEventListener('touchmove', function (evt) {
+            if (el.offsetHeight < el.scrollHeight) {
+                evt._isScroller = true;
+            }
+        });
+    }
+    document.addEventListener('touchstart', function (event) {
+        event.preventDefault();
+    });
